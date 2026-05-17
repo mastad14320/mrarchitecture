6fdc7259cc6d4316230bb54f54f36ef42f593038
mastad14320 <mastad14320@gmail.com>
2026-05-17 21:23:36 +0300

    Update plans.js

--- assets/js/plans.js
+++ assets/js/plans.js
@@ -1,6 +1,6 @@
 import {
   db
-} from "/assets/js/firebase.js";
+} from "./firebase.js";
 
 import {
   
@@ -13,7 +13,7 @@ import {
 import {
   toggleFavorite, 
   isFavorite
-} from "/assets/js/favorites.js";
+} from "./favorites.js";
 
 import {
 
@@ -21,7 +21,7 @@ import {
 
   isInCart
 
-} from "/assets/js/cart.js";
+} from "./cart.js";
 
 // GRID
 const plansGrid =
